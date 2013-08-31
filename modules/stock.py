31d6bede8344bee3780de37b915d3ba08dc91ac9
Aaron Crosley <acrosley108@gmail.com>
2013-08-31 18:07:39 -0400

    Add stock.py

---
+++ modules/stock.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+stock.py - Phenny ticker checker
+author: Aaron Crosley <acrosley108@gmail.com>
+
+"""
+
+def stock(phenny, input):
+	continue
+
+
+
+
+stock.commands = ['stock']
+stock.example = '.stock AAPL'
+
+if __name__ == '__main__':
+    print(__doc__.strip())
