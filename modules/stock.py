b68d578b16e60b28202fe9bac7599c769cfcc512
Aaron Crosley <acrosley108@gmail.com>
2013-08-31 19:21:40 -0400

    stock.py first try

--- modules/stock.py
+++ modules/stock.py
@@ -4,11 +4,23 @@ stock.py - Phenny ticker checker
 author: Aaron Crosley <acrosley108@gmail.com>
 
 """
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+
+def _request(symbol, stat):
+    url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (symbol, stat)
+    req = Request(url)
+    resp = urlopen(req)
+    return str(resp.read().decode('utf-8').strip())
+
+
+def get_price(symbol):
+    return _request(symbol, 'l1')
+
 
 def stock(phenny, input):
-	continue
-
-
+	quote = get_price(input)
+	phenny.say(quote)
 
 
 stock.commands = ['stock']
